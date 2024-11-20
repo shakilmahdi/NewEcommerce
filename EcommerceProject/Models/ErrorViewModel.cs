51d1fe487ebd46fc687c19fb3f70a3eb2351fdc3
Shakil <19103040@iubat.edu>
2024-11-20 22:38:11 +0600

    First

---
+++ EcommerceProject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EcommerceProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
